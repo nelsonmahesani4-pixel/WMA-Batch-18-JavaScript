276934d8ee527da0fba32ac09eb3c42d3db0786b
nelsonmahesani <nelsonmahesani4@gmail.com>
2025-11-25 21:08:17 +0500

    alert and Variables

---
+++ ECMA Script/index.js
@@ -0,0 +1,32 @@
+
+// alert -- 1st chapter
+// alert("Hell0 World!");
+// console.log("I'm Nelson");
+
+// 2nd chapter---> variables
+// // var It is like a container to store data
+var myName="Nelson Rathore";
+console.log(myName);
+var age=22;
+var totalAge=age+8;
+console.log(totalAge);
+
+// variable-string 
+var name="Nelsonrathore";
+console.log (name);
+
+// // variable-number
+
+// // variable-familiar
+var weight=45;
+console.log(weight);
+
+var age=17;
+var newAge=2;
+var totalAge=age+newAge;
+console.log(totalAge);
+
+// variable-unfamiliar
+var num=1;
+var newNum=++num + ++num + ++num + num++ ;
+console.log(newNum);
