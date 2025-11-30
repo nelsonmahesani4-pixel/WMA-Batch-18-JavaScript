35be7feadc96cc2d1ae41d298119d916010d0da8
nelsonmahesani <nelsonmahesani4@gmail.com>
2025-11-30 22:18:29 +0500

    If else-conditions & prompts

--- ECMA Script/index.js
+++ ECMA Script/index.js
@@ -5,78 +5,210 @@
 
 // 2nd chapter---> variables
 // // var It is like a container to store data
-var myName="Nelson Rathore";
-console.log(myName);
-var age=22;
-var totalAge=age+8;
-console.log(totalAge);
+// var myName="Nelson Rathore";
+// console.log(myName);
+// var age=22;
+// var totalAge=age+8;
+// console.log(totalAge);
 
-// variable-string 
-var name="Nelsonrathore";
-console.log (name);
+// // variable-string 
+// var name="Nelsonrathore";
+// console.log (name);
 
-// // variable-number
+// // // variable-number
 
-// // variable-familiar
-var weight=45;
-console.log(weight);
+// // // variable-familiar
+// var weight=45;
+// console.log(weight);
 
-var age=17;
-var newAge=2;
-var totalAge=age+newAge;
-console.log(totalAge);
+// var age=17;
+// var newAge=2;
+// var totalAge=age+newAge;
+// console.log(totalAge);
 
-// variable-unfamiliar
-var num=1;
-var newNum=++num + ++num + ++num + num++ ;
-console.log(newNum);
+// // variable-unfamiliar
+// var num=1;
+// var newNum=++num + ++num + ++num + num++ ;
+// console.log(newNum);
 
-var a=1;
-var b=1;
-var sum=a++ + b++ + a++ + b++ + ++a + ++b;
-// sum=1+1+2+2+4+4
-console.log(sum);
-// alert("Hello,World");
-var a=1;
-var b=1;
-var sum=++a + b++ + ++a + ++b; 
-console.log(sum);
-console.log("Hello World!");
+// var a=1;
+// var b=1;
+// var sum=a++ + b++ + a++ + b++ + ++a + ++b;
+// // sum=1+1+2+2+4+4
+// console.log(sum);
+// // alert("Hello,World");
+// var a=1;
+// var b=1;
+// var sum=++a + b++ + ++a + ++b; 
+// console.log(sum);
+// console.log("Hello World!");
 
-// var name=  promt("Enter your name");
-// alert( name);
+// // var name=  promt("Enter your name");
+// // alert( name);
 
 
-alert("Hello");
+// alert("Hello");
 
-var uname="ali";
-var age=20;
-if (uname =="ali"){
-    if (age ==21){
+// var uname="ali";
+// var age=20;
+// if (uname =="ali"){
+//     if (age ==21){
+//     }
+// else{
+//     console.log("Don't allow")
+// }
+// }
+
+
+// // Array //
+// // what is data type
+// // "ali" string
+// // 234 number
+// //  ture/false --> bolean variable
+// // undefined -->  abhi mene data nahin rakha
+// // var name = nul -->  mtlb data to rakh le h lekin abhi nahin;
+
+// // array
+// var city1="karachi";
+// var city2="Mirpurkhas";
+// var city2="Hyderabad";
+
+// var cities = ["karachi", "Mirpurkhas", "Hyderabad"]; //-->   // this is called array//
+
+// //index(unique id of an element ) -> //id-start-from (0)
+// // length -> quantity of an element
+// // console.log (type of cities) //object
+// console.log(cities);
+// console.log(cities[0]);
+
+// var units= 2 + (2*4) + 10;
+// console.log(units)
+
+// var spec = prompt("Your species?", "human");
+
+
+// var a=2
+
+// var decre= --a 
+// console.log(decre)
+
+// var msg="Most Welcome!"
+// console.log(msg + "Nelson")
+
+// PROMPT-CHAPTER-9
+
+// alert (just for message)
+// prompt (message with user information)
+
+
+// prompt("hi","Nelson")
+// var userName ="NELSON"
+// var message ="you are handsome"
+// var result = prompt(userName,message)
+
+// var number1 =Number(prompt("enter first number"))
+// var number2 =+prompt("enter second number")
+
+// var result = number1 + number2
+
+// console.log(result)
+
+// var prompt = require('prompt-sync')();
+
+// var number  =prompt("enter a number")
+// var sum =number + 1
+// console.log("sum=>", sum)
+
+
+
+// chapter-10   
+// If -else Statements
+
+// if(false) {
+//     console.log("cond is true")
+
+// }
+
+// console.log("outside Cond")
+// = assign
+// ==--> condition
+// === condition with type (string, number)
+
+// var uname =prompt("enter your name") 
+//     var absentStudent=prompt("enter absent student");
+// if (uname === absentStudent){
+//     console.log(uname +"you are not allow")
+// }
+// else{
+// console.log(uname +" you are allow")
+// }
+
+
+// --comparision and If-else
+
+// var num1=2
+// var num2=3
+// if(num2>num1) {
+//     console.log(num2 +"is greater than" +num1)
+// }
+
+// if-else else-if
+
+// var uname =prompt("enter your name");
+
+// if(uname ==="ali") {
+//     console.log(" You were absent");
+// } else if(uname ==="asad") {
+//     console.log(" You were absent");
+// } else if(uname ==="bilal") {
+//     console.log(" You were absent");
+// } 
+// else{
+//     console.log("you were present");
+// }
+
+// var stdName=prompt("enter your name")
+// var stdAge=Number(prompt("enter your age"))
+// var name="ali"
+// var age=16
+// if(stdName === name || age ===age) {
+// console.log("you passed matric")
+// }
+
+// var weight=45
+// var time=6
+// if ( weight >40 && time<=6){
+//     alert("Come to our tryout");
+// }
+// else{
+//     alert("come to our cookout!");
+// }
+var a=1
+var b=7
+var c=2
+var d=2
+var x=5
+var y=6
+var g=1
+var h=0
+var e=11
+var f=12
+
+if(c ===d){
+    if(x === y){
+        console.log("g=h");
     }
-else{
-    console.log("Don't allow")
+    else if(a ===b){
+        console.log("h=g");
+    }
+
+    else{
+        console.log("e=f");
+    }
 }
-}
 
 
-// Array //
-// what is data type
-// "ali" string
-// 234 number
-//  ture/false --> bolean variable
-// undefined -->  abhi mene data nahin rakha
-// var name = nul -->  mtlb data to rakh le h lekin abhi nahin;
+// Array
+// pop=shift} are used to remove an element from end and beggining respect:  --> methods
+// push =unshift} are used to add  an element to the end and beggining respect:   --> methods
 
-// array
-var city1="karachi";
-var city2="Mirpurkhas";
-var city2="Hyderabad";
-
-var cities = ["karachi", "Mirpurkhas", "Hyderabad"]; //-->   // this is called array//
-
-//index(unique id of an element ) -> //id-start-from (0)
-// length -> quantity of an element
-// console.log (type of cities) //object
-console.log(cities);
-console.log(cities[0]);
