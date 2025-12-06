65e94c33e8df3a2e7cc309484334aec680a37579
nelsonmahesani <nelsonmahesani4@gmail.com>
2025-12-06 21:11:19 +0500

    JavaScript Assignment (1-11)

--- ECMA Script/JavaScript Exercise 1 to 10/index2.js
+++ ECMA Script/JavaScript Exercise 1 to 10/index2.js
@@ -1,81 +1,104 @@
-// (Exercise 1) Alert
+// // (Exercise 1) Alert
 
-alert("Hello, World!")
-alert("Nelson Rathore Mahesani")
-alert(5)
+// alert("Hello, World!")
+// alert("Nelson Rathore Mahesani")
+// alert(5)
 
-var firstName = "Nelson";
-alert(firstName)
+// var firstName = "Nelson";
+// alert(firstName)
 
-var name = "Nelson";
-alert("Hello " + name);
+// var name = "Nelson";
+// alert("Hello " + name);
 
-// (Exercise 2) variables for Strings
+// // (Exercise 2) variables for Strings
 
-var  userName = "Nelson";
-console.log(userName);
-var greetingMsg = "Welcome";
-alert(greetingMsg );
+// var  userName = "Nelson";
+// console.log(userName);
+// var greetingMsg = "Welcome";
+// alert(greetingMsg );
 
-var firstName = "Nelson ";
-var lastName = "Rathore";
-var fullName = firstName + lastName ;
-console.log(fullName)
+// var firstName = "Nelson ";
+// var lastName = "Rathore";
+// var fullName = firstName + lastName ;
+// console.log(fullName)
 
-var myFavQuote = "I don’t write code to impress people — I write it to improve myself.";
-alert(myFavQuote)
-console.log(myFavQuote);
+// var myFavQuote = "I don’t write code to impress people — I write it to improve myself.";
+// alert(myFavQuote)
+// console.log(myFavQuote);
 
-var city = "Welcomr to [Karachi]!";
-alert(city);
+// var city = "Welcomr to [Karachi]!";
+// alert(city);
 
-// (exercise 3) Variables for numbers
-// var age = 17;
+// // (exercise 3) Variables for numbers
+// // var age = 17;
 
-var num1 = 50;
-var num2 = 55;
-var sum = num1 + num2 ;
-console.log(sum)
-var subtract = num2 - num1 ;
-console.log (subtract)
-var multiPly = num1 * num2 ;
-console.log(multiPly)
+// var num1 = 50;
+// var num2 = 55;
+// var sum = num1 + num2 ;
+// console.log(sum)
+// var subtract = num2 - num1 ;
+// console.log (subtract)
+// var multiPly = num1 * num2 ;
+// console.log(multiPly)
 
-var age = 20;
-console.log("You are 20 years old")
+// var age = 20;
+// console.log("You are 20 years old")
 
 
-// (exercise 4) Math expressions
-var x = 10;
-var y = 5;
+// // (exercise 4) Math expressions
+// var x = 10;
+// var y = 5;
 
-var totalSum = x + y ;
-console.log(totalSum)
+// var totalSum = x + y ;
+// console.log(totalSum)
 
-var subtract = y - x ;
-// alert(subtract);
+// var subtract = y - x ;
+// // alert(subtract);
 
-var multiply = x * y;
-console.log(multiply);
+// var multiply = x * y;
+// console.log(multiply);
 
-var divide = x / y ;
-console.log(divide )
+// var divide = x / y ;
+// console.log(divide )
 
-var riminder = x % y ;
-console.log(riminder);
+// var riminder = x % y ;
+// console.log(riminder);
 
-// (exercise 5) Prompt
+// // (exercise 5) Prompt
 
-var userAge = prompt("enter your age ")
-console.log(userAge);
+// var userAge = prompt("enter your age ")
+// console.log(userAge);
 
-var userNum = Number(prompt("enter number "))
-console.log(userNum + userNum++);
+// var userNum = Number(prompt("enter number "))
+// console.log(userNum + userNum++);
 
-var color = prompt("what is your favorite color")
+// var color = prompt("what is your favorite color")
 
-var blueColor = prompt(color , "Wow! I like blue too!")
-console.log(blueColor)
+// var blueColor = prompt(color , "Wow! I like blue too!")
+// console.log(blueColor)
+
+
+// EXERCISE (6) If and comparison Operators
+var age = 29;
+if(age < "30"){
+    console.log("you're still Young man.")
+}
+
+var number = prompt("enter a number");
+if(number % 2 == 0) {
+console.log("Even Number")
+}
+
+// comparison Operators
+
+var x = 5 ;
+var y = 7;
+if (y < x ) {
+    console.log("7 is greater than 5");
+}
+
+
+
 
 
 
