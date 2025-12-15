f4ab1fb1f95cab1d685ab1c1e22fbb3a6967453e
nelsonmahesani <nelsonmahesani4@gmail.com>
2025-12-15 10:53:43 +0500

    Functions

--- ECMA Script/15-12-2025/index.js
+++ ECMA Script/15-12-2025/index.js
@@ -29,20 +29,59 @@
 // funct(myName)  //Argument
 
 
-function funct(lName,myName){ // Parameter
-    console.log(myName,lName)
-}
-var  myName = "Nelson"
-funct(myName,"Rathore")  //Argument
-
-function funct(lName,myName){ // Parameter
-    console.log(myName,lName)
-}
+// function funct(lName,myName){ // Parameter
+//     console.log(myName,lName)
+// }
 // var  myName = "Nelson"
-funct("Nelson","Rathore")  //Argument
+// funct(myName,"Rathore")  //Argument
+
+// function funct(lName,myName){ // Parameter
+//     console.log(myName,lName)
+// }
+// // var  myName = "Nelson"
+// funct("Nelson","Rathore")  //Argument
 
 
-function sum(a,b) {
-    console.log("sum=>", a+b)
-}
-sum(2,3)
+// function sum(a,b) {
+    
+//     console.log("A", a)
+//     console.log("B", b)
+//     console.log("sum=>", a ** b)
+// }
+// sum(2,3)
+
+// function add(a,b = 2) {        // add(a,b = 2) --> this is called default parameter
+//     alert(a + b)
+// }
+// add(2,7)
+
+
+// function number(a,b) {
+//     // return(a + b)
+//    return a + b
+// }
+
+// console.log(number(10,3))
+
+// var myName = "Nelson"  // Global variable  /variable created outside of  function
+// function test(myName) {
+// console.log(myName)
+// }
+// test(myName)
+
+
+//   var name = "first Name"
+// function fun() {
+//     console.log(name);
+// }
+// var name = "name changed"
+// fun() 
+
+// console.log(uname)
+// var uname = "ali"
+
+// if,for (block scope)
+//variable  (local/function scope)
+console.log(uname)  
+const uname = "ali"  // TDZ -->  temporal dead zone
+
