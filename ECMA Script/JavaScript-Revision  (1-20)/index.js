6f2911caa59d402f060ecec66e18c9883a7e70ca
nelsonmahesani <nelsonmahesani4@gmail.com>
2025-12-17 10:59:17 +0500

    JavaScript Revision (1-20)

---
+++ ECMA Script/JavaScript-Revision  (1-20)/index.js
@@ -0,0 +1,36 @@
+// BOM   --> Browser object model
+// Read only(OK)
+
+// DOM  -->
+
+// Alert
+// window.alert("Hi")
+// alert("")
+
+// variables  -->  greek word     vary-(change) and ables(able)
+
+ var name = "Nelson";   // --> assign (Nelson)  
+ const myName = "Nelson";
+
+ console.log(myName,name)
+
+ // null is the value 
+ // undefined  data type
+
+//  var var = "NM"  // reserved
+// variable name conventions
+
+//1. pascal case
+//2. camel case
+//3. snake case
+//4.train case
+// console.log(firstName)  // temporal dead zone  (don't use (console.log) before decalaration/initialization a variable)
+// let firstName = "Rthore";
+
+
+ 
+
+
+
+
+
