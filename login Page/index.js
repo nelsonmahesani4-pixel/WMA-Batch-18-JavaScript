c768a6d88818682356e212132f87267e42e42d36
nelsonmahesani <nelsonmahesani4@gmail.com>
2026-01-06 19:59:21 +0500

    updated

---
+++ login Page/index.js
@@ -0,0 +1,11 @@
+function action() {
+    document.querySelector('.login-form').style.display = 'none';
+    document.querySelector('.signup-form').style.display = 'block';
+
+
+    document.querySelector('.container').style.display = 'none';
+    document.querySelector('.container2').style.display = 'block';
+
+    
+
+}
